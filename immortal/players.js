261f4e1509d9885424a29caa082e9c5d5cfbd11a
鱼喵 <yumiao_qwq@163.com>
2026-04-09 23:09:47 +0800

    Update players.js

--- immortal/players.js
+++ immortal/players.js
@@ -1,4 +1,4 @@
 const playerList = [
     { id: "XXX", name: "稻子解说", avatar: "https://s1.img-e.com/20260409/69d7b3d472074.png" },
-    { id: "XXX", name: "特别痒的正是", avatar: "https://s1.img-e.com/20260409/69d7b3d458603.jpg" }
+    { id: "755901134", name: "特别痒的正是", avatar: "https://s1.img-e.com/20260409/69d7b3d458603.jpg" }
 ];
