582ed23f9c7c4e2950becfbde803566e81e6a6f4
鱼喵 <2825924703@qq.com>
2026-04-12 00:42:00 +0800

    Update players.js

--- immortal/players.js
+++ immortal/players.js
@@ -1,5 +1,5 @@
 const playerList = [
     { id: "不详（欢迎投稿）", name: "稻子解说", avatar: "https://s1.img-e.com/20260409/69d7b3d472074.png" },
     { id: "755901134", name: "特别痒的正是", avatar: "https://s1.img-e.com/20260409/69d7b3d458603.jpg" },
-    { id:"3965943969",name:"甜瓜",avatar:"https://q1.qlogo.cn/g?b=qq&nk=3965943969&s=640"
+    { id:"3965943969",name:"甜瓜",avatar:"https://q1.qlogo.cn/g?b=qq&nk=3965943969&s=640"}
 ];
